942794379044ab1005b63295a831e6457d246ea4
ndidodley <88383651+ndidodley@users.noreply.github.com>
2021-08-06 00:45:47 +0100

    Added input form to main page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let consolidate = require('consolidate');
 let path = require('path');
 let axios = require('axios').default;
 let bodyParser = require('body-parser');
-require('dotenv').config({path:__dirname + '/.env'});
+require('dotenv').config({path: __dirname + '/.env'});
 let logger = require('morgan');
 
 let app = express();
@@ -19,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(logger('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname, +'/public'));
 
 
@@ -30,12 +31,12 @@ axios.create({
 })
 
 
-async function getImages(query, limit, orientation) {
+async function getImages(query, limit) {
     try {
         return await axios.get('https://api.unsplash.com/search/photos', {
             params: {
                 query: query,
-                per_page: limit,
+                per_page: limit || 10,
 
                 client_id: access_key
 
@@ -47,16 +48,39 @@ async function getImages(query, limit, orientation) {
 }
 
 app.get('/', (req, res) => {
-    getImages('dalmatian' , 15, 'portrait').then(
+    getImages('dalmatian', 15, 'portrait').then(
         (x) => {
             try {
                 let results = x.data;
                 return res.render('index', results);
-            }catch (e) {
+            } catch (e) {
                 console.log(`The following error(s) occured ${e}`);
             }
         }
     );
+})
+
+app.post('*', (req, res, next) => {
+    let query = req.body.query;
+    let limit = req.body.limit;
+    console.log(query, limit);
+    getImages(query, limit, 'potrait').then(
+        (x) => {
+            try {
+                if (x === undefined) {
+                    return next();
+                } else {
+                    let results = x.data;
+                    return res.render('index', results);
+                }
+            } catch (e) {
+                console.log(`The following errors occurred ${e}`);
+                return next(e);
+            }
+        }
+    )
+}, function (req, res) {
+    return res.render('index')
 })
 
 app.listen(3000, () => {
